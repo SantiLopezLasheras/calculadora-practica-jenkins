b2274914fcce36f90a93d06c6f1a72f0325d886d
santijll <santijll@example.com>
2025-01-18 18:37:58 +0100

    añadiendo stage 6 - push changes

--- jenkinsScripts/pushChanges.sh
+++ jenkinsScripts/pushChanges.sh
@@ -1,13 +1,6 @@
-echo ${env.GITHUB_USERNAME}
-echo ${env.GITHUB_TOKEN}
-echo ${env.GITHUB_EMAIL}
-echo ${env.executor}
-echo ${env.motiu}
-git remote set-url origin https://${env.GITHUB_USERNAME}:${env.GITHUB_TOKEN}@github.com/SantiLopezLasheras/calculadora-practica-jenkins.git
-git config --global user.email ${env.GITHUB_EMAIL}
-git config --global user.name ${env.GITHUB_USERNAME}
+git remote set-url origin https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/SantiLopezLasheras/calculadora-practica-jenkins.git
 git fetch origin
 git add .
-git status
 git commit -m "Pipeline executada per ${env.executor}. Motiu: ${env.motiu}"
-git push -v origin HEAD:main
+git status
+git push -v origin HEAD:ci_jenkins
